4947ccf8d7a192bd4f6522849abbd04905c030fc
Supriya <sj2685@nyu.edu>
2019-06-03 18:32:00 -0400

    Update grocerynew.py

---
+++ grocerynew.py
@@ -0,0 +1,5 @@
+products = [
+    {"id":1, "name": "Chocolate Sandwich Cookies", "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
+    {"id":2, "name": "All-Seasons Salt", "department": "pantry", "aisle": "spices seasonings", "price": 4.99}
+]
+print(products)
